e2204baccec65527fbba7266231c713de3b5c813
Unknown <gil.daniel19@hotmail.com>
2018-03-01 13:20:42 -0500

    complemento ejemplo3

--- ejemploswitch3/main.cpp
+++ ejemploswitch3/main.cpp
@@ -4,12 +4,20 @@ using namespace std;
 //funcion principal
 int main()
 {
-    int dia=0,mes=0,anio=0,dato=0,total=0;// declara variables
+    int dia=0,mes=0,anio=0,dato=0,total=0,R=0;// declara variables
     cout << "ingresar una fecha de modo dia mes año como un numero entero" << endl;
     cin >> dato;
+    R=(dato%1000000);
+    if (dato<=31999999){
     total=dato/1000000;
     mes=(dato/10000)%100;
+    }
+    if (R<129999){
     anio=(dato%10000);
+    }
+    else {
+        cout << "dato erroneo " << endl;
+    }
     cout << "es "<<total<<" del mes "<<mes<<" del año "<<anio<<endl;
     return 0;
 }
